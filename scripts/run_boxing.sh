96b01286702436e08a75cadf83eaa066ceaf8306
UsanoCoCr <2200017416@stu.pku.edu.cn>
2026-03-24 15:50:34 +0800

    initial my repo

--- scripts/run_boxing.sh
+++ scripts/run_boxing.sh
@@ -1,8 +1,13 @@
 export OMP_NUM_THREADS=1
-python phc/run_hydra.py     project_name=SMPLOlympics num_agents=2     \
-        learning=amp_z_self_play exp_name=boxing_pulse \
+python phc/run_hydra.py project_name=SMPLOlympics num_agents=2     \
+        learning=amp_z_self_play exp_name=boxing_amp \
         env=env_amp_z env.num_envs=2048 env.task=HumanoidBoxingZ env.enableTaskObs=True  \
-        env.stateInit=Start    robot=smpl_humanoid_boxing +env.models=["output/HumanoidIm/pulse_vae_iclr/Humanoid.pth"]      \
-        env.motion_file=./sample_data/video_boxing_afterproc_upright.pkl headless=True env.episode_length=300  learning.params.config.switch_frequency=250
+        env.stateInit=Start robot=smpl_humanoid_boxing +env.models=["output/HumanoidIm/pulse_vae_iclr/Humanoid.pth"]      \
+        env.motion_file=./sample_data/video_boxing_afterproc_upright.pkl headless=True env.episode_length=300 learning.params.config.switch_frequency=250
 
+# python phc/run_hydra.py project_name=SMPLOlympics num_agents=2     \
+#         learning=ppo exp_name=boxing_ppo \
+#         env=env_amp_z env.num_envs=2048 env.task=HumanoidBoxingZ env.enableTaskObs=True  \
+#         env.stateInit=Start robot=smpl_humanoid_boxing +env.models=["output/HumanoidIm/pulse_vae_iclr/Humanoid.pth"]      \
+#         env.motion_file=./sample_data/video_boxing_afterproc_upright.pkl headless=True env.episode_length=300 learning.params.config.switch_frequency=250
 
